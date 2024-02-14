4010e8eac1f775dbee0c6ae630a59b14d7917551
leonard-porteria <porterialeonard@gmail.com>
2024-02-14 22:18:23 +0800

    remove jupyter notebook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,26 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { ModeToggle } from './components/mode-toggle';
 
+import { Button } from './components/ui/button';
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+
 function App() {
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <ModeToggle />
+      <div>
+        <h1>Credit Score Classifier</h1>
+        <div>
+          <Button variant='outline' size='icon' asChild>
+            <a
+              href='https://github.com/leonardporteria/credit-score-classifier'
+              target='_blank'
+            >
+              <GitHubLogoIcon className='h-4 w-4' />
+            </a>
+          </Button>
+          <ModeToggle />
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
